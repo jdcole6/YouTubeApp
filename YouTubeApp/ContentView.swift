3a972c34ba703bbce190b9ec106ac2224dcf8f09
Jack Cole <jackdcole@gmail.com>
2024-06-28 18:25:45 -0400

    Initial xcode project

---
+++ YouTubeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouTubeApp
+//
+//  Created by Jack Cole on 6/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouTubeApp/YouTubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeAppApp.swift
+//  YouTubeApp
+//
+//  Created by Jack Cole on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
